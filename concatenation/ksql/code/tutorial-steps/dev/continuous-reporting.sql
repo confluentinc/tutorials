ab41eb189887b1e119e8f5a04cdf657d577bebe3
Bill Bejeck <bbejeck@gmail.com>
2023-12-13 15:55:05 -0500

    Conversion of concatenation tutorial

--- concatenation/ksql/code/tutorial-steps/dev/continuous-reporting.sql
+++
@@ -1,7 +0,0 @@
-CREATE STREAM SUMMARY_RESULTS AS
-  SELECT FIRST_NAME + ' ' + LAST_NAME +
-       ' purchased ' +
-       CAST(NUM_SHARES AS VARCHAR) +
-       ' shares of ' +
-       SYMBOL AS SUMMARY
-FROM ACTIVITY_STREAM;
--- concatenation/ksql/code/tutorial-steps/dev/init.sh
+++
@@ -1,1 +0,0 @@
-mkdir concatenation && cd concatenation
