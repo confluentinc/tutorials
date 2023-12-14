fa2da438596c2db578ef1493036324549c05e09d
Dave Troiano <dtroiano@confluent.io>
2023-12-14 16:03:15 -0500

    simplify versioned KTables tutorial

--- versioned-ktables/kstreams/code/tutorial-steps/dev/harness-console-consumer-verify-table.sh
+++
@@ -1,7 +0,0 @@
-docker exec -it broker /usr/bin/kafka-console-consumer \
- --topic table-input-topic \
- --bootstrap-server broker:9092 \
- --from-beginning \
- --property print.key=true \
- --property key.separator=" : " \
- --max-messages 10
