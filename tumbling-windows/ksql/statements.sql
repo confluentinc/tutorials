d09e3e14d07e5b3ec3eb3d50ee66c4d8206bfea0
Dave Troiano <dtroiano@confluent.io>
2024-01-08 16:12:08 -0500

    convert tumbling windows ksqlDB tutorial

--- tumbling-windows/ksql/statements.sql
+++ tumbling-windows/ksql/statements.sql
@@ -1,3 +1,10 @@
+CREATE STREAM ratings (title VARCHAR, release_year INT, rating DOUBLE, timestamp VARCHAR)
+    WITH (kafka_topic='ratings',
+          timestamp='timestamp',
+          timestamp_format='yyyy-MM-dd HH:mm:ss',
+          partitions=1,
+          value_format='avro');
+
 INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('Die Hard', 1998, 8.2, '2019-07-09 01:00:00');
 INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('Die Hard', 1998, 4.5, '2019-07-09 05:00:00');
 INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('Die Hard', 1998, 5.1, '2019-07-09 07:00:00');
@@ -13,3 +20,15 @@ INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('The Big Le
 INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('The Big Lebowski', 1998, 4.2, '2019-07-09 02:00:00');
 
 INSERT INTO ratings (title, release_year, rating, timestamp) VALUES ('Super Mario Bros.', 1993, 3.5, '2019-07-09 18:00:00');
+
+SET 'auto.offset.reset' = 'earliest';
+
+SELECT title,
+       COUNT(*) AS rating_count,
+       WINDOWSTART AS window_start,
+       WINDOWEND AS window_end
+FROM ratings
+         WINDOW TUMBLING (SIZE 6 HOURS)
+GROUP BY title
+    EMIT CHANGES
+    LIMIT 11;
