a29b7058334fa892ae3255d699c400f797eb6e40
Dave Troiano <dtroiano@confluent.io>
2024-01-19 09:39:34 -0500

    remove better-monads dependency from window final result tutorial

--- window-final-result/kstreams/src/main/java/io/confluent/developer/PressureDatetimeExtractor.java
+++ window-final-result/kstreams/src/main/java/io/confluent/developer/PressureDatetimeExtractor.java
@@ -1,6 +1,5 @@
 package io.confluent.developer;
 
-import com.jasongoodwin.monads.Try;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.processor.TimestampExtractor;
 import org.slf4j.Logger;
@@ -8,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 
 public class PressureDatetimeExtractor implements TimestampExtractor {
@@ -21,13 +21,17 @@ public class PressureDatetimeExtractor implements TimestampExtractor {
 
     @Override
     public long extract(ConsumerRecord<Object, Object> record, long previousTimestamp) {
-        return Try
-                .ofFailable(() -> ((PressureAlert) record.value()).dateTime())
-                .onFailure((ex) -> logger.error("fail to cast the PressureAlert: ", ex))
-                .map((stringDatetimeString) ->  ZonedDateTime.parse(stringDatetimeString, this.formatter))
-                .onFailure((ex) -> logger.error("fail to parse the event datetime due to: ", ex))
-                .map((zonedDatetime) -> zonedDatetime.toInstant().toEpochMilli())
-                .onFailure((ex) -> logger.error("fail to convert the datetime to instant due to: ", ex))
-                .orElse(-1L);
+        try {
+            String dateTimeString = ((PressureAlert) record.value()).dateTime();
+            ZonedDateTime zonedDateTime = ZonedDateTime.parse(dateTimeString, this.formatter);
+            return zonedDateTime.toInstant().toEpochMilli();
+        } catch (ClassCastException cce) {
+            logger.error("failed to cast the PressureAlert: ", cce);
+        } catch (DateTimeParseException dtpe) {
+            logger.error("fail to parse the event datetime due to: ", dtpe);
+        }
+
+        // Returning a negative number will cause records to be skipped
+        return -1L;
     }
 }
