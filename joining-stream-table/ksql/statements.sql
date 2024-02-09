7b5ea0ff41f0f9e465466e7acfb4777ee275858b
Bill Bejeck <bbejeck@gmail.com>
2024-02-09 11:39:29 -0500

    ... And one more that I missed

--- joining-stream-table/ksql/statements.sql
+++ joining-stream-table/ksql/statements.sql
@@ -13,7 +13,7 @@ CREATE TABLE movies (id INT PRIMARY KEY, title VARCHAR, release_year INT)
 CREATE STREAM rated_movies
     WITH (kafka_topic='rated_movies',
           value_format='avro') AS
-SELECT ratings.movie_id as id, title, rating
+SELECT ratings.movie_id AS id, title, rating
 FROM ratings
          LEFT JOIN movies ON ratings.movie_id = movies.id;
 
