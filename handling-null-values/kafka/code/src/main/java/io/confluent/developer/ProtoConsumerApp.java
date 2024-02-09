4d9e38d61dcf168a6d2e95d1dd9d2096e1bad427
Cerchie <lcerchie@confluent.io>
2024-02-09 07:21:21 -0700

    first code batch

---
+++ handling-null-values/kafka/code/src/main/java/io/confluent/developer/ProtoConsumerApp.java
@@ -0,0 +1,78 @@
+package io.confluent.developer;
+
+import io.confluent.developer.proto.PurchaseProto.Purchase;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializerConfig;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+
+public class ProtoConsumerApp {
+
+    public void close() {
+        ExecutorService executorService = null;
+        executorService.shutdown();
+    }
+
+    public ConsumerRecords<String, Purchase>  consumePurchaseEvents()  {
+         Properties properties = loadProperties();
+         Map<String, Object> protoConsumerConfigs = new HashMap<>();
+
+         properties.forEach((key, value) -> protoConsumerConfigs.put((String) key, value));
+         protoConsumerConfigs.put(ConsumerConfig.GROUP_ID_CONFIG, "schema-registry-course-consumer");
+         protoConsumerConfigs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+         protoConsumerConfigs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+         protoConsumerConfigs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class);
+         protoConsumerConfigs.put(KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE, Purchase.class);
+
+         // Duplication of configs loaded from confluent.properties to emphasize what's needed to use SchemaRegistry
+         protoConsumerConfigs.put("schema.registry.url", "SR_URL");
+         protoConsumerConfigs.put("basic.auth.credentials.source", "USER_INFO");
+         protoConsumerConfigs.put("basic.auth.user.info", "KEY:SECRET");
+
+
+
+         Consumer<String, Purchase> protoConsumer = new KafkaConsumer<>(protoConsumerConfigs);
+         protoConsumer.subscribe(Collections.singletonList("proto-purchase"));
+
+         ConsumerRecords<String, Purchase> protoConsumerRecords = protoConsumer.poll(Duration.ofSeconds(2));
+         protoConsumerRecords.forEach(protoConsumerRecord -> {
+             Purchase protoPurchase = protoConsumerRecord.value();
+             System.out.print("Purchase details consumed from topic with Protobuf schema { ");
+             System.out.printf("Customer: %s, ", protoPurchase.getCustomerId());
+             System.out.printf("Total Cost: %f, ", protoPurchase.getTotalCost());
+             System.out.printf("Item: %s } %n", protoPurchase.getItem());
+         });
+            return protoConsumerRecords;
+     }
+
+
+         Properties loadProperties () {
+             try (InputStream inputStream = this.getClass()
+                     .getClassLoader()
+                     .getResourceAsStream("confluent.properties")) {
+                 Properties props = new Properties();
+                 props.load(inputStream);
+                 return props;
+             } catch (IOException exception) {
+                 throw new RuntimeException(exception);
+             }
+         }
+
+         public static void main (String[]args){
+             ProtoConsumerApp consumerApp = new ProtoConsumerApp();
+             consumerApp.consumePurchaseEvents();
+         }
+     }
---
+++ handling-null-values/kafka/code/src/test/java/io/confluent/developer/ProtobufProduceConsumeAppTest.java
@@ -0,0 +1,129 @@
+package io.confluent.developer;
+
+
+import io.confluent.developer.proto.PurchaseProto;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.MockConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.clients.producer.MockProducer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.KeyValue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProtobufProduceConsumeAppTest {
+    private static final Map<String, Object> commonConfigs = new HashMap<>();
+    private static final Properties properties = new Properties();
+    private final Serializer<String> stringSerializer = new StringSerializer();
+    private ProtoProducerApp protoProducerApp;
+    private ProtoConsumerApp protoConsumerApp;
+
+    @BeforeClass
+    public static void beforeAllTests() throws IOException {
+        try (FileInputStream fis = new FileInputStream("configuration/test.properties")) {
+            properties.load(fis);
+            properties.forEach((key, value) -> commonConfigs.put((String) key, value));
+        }
+    }
+
+
+    @Before
+    public void setup() {
+        protoProducerApp = new ProtoProducerApp();
+        protoConsumerApp = new ProtoConsumerApp();
+    }
+
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testProduceProtoMultipleEvents() {
+        KafkaProtobufSerializer protoSerializer
+                = new KafkaProtobufSerializer();
+        protoSerializer.configure(commonConfigs, false);
+        MockProducer<String, SpecificRecordBase> mockProtoProducer
+                = new MockProducer<String, SpecificRecordBase>(true, stringSerializer, (Serializer) protoSerializer);
+
+        List<PurchaseProto.Purchase> actualKeyValues = protoProducerApp.producePurchaseEvents();
+
+        List<PurchaseProto.Purchase> returnedAvroResults = protoProducerApp.producePurchaseEvents();
+
+        List<KeyValue<String, SpecificRecordBase>> expectedKeyValues =
+                mockProtoProducer.history().stream().map(this::toKeyValue).collect(Collectors.toList());
+
+        returnedAvroResults.forEach(c ->
+        {
+            String purchaseProtoId = c.getCustomerId();
+            assertEquals("Customer Null", purchaseProtoId);
+            assertEquals(actualKeyValues.size(), 2);
+        });
+
+    }
+
+    @Test
+    public void testConsumeProtoEvents() {
+        MockConsumer<String, PurchaseProto.Purchase> mockConsumer = new MockConsumer<>(OffsetResetStrategy.LATEST);
+        String topic = (String) commonConfigs.get("proto.topic");
+
+        mockConsumer.schedulePollTask(() -> {
+            addTopicPartitionsAssignment(topic, mockConsumer);
+            addConsumerRecords(mockConsumer, protoProducerApp.producePurchaseEvents(), PurchaseProto.Purchase::getCustomerId, topic);
+        });
+
+        ConsumerRecords<String,PurchaseProto.Purchase> returnedProtoResults = protoConsumerApp.consumePurchaseEvents();
+        List<PurchaseProto> actualProtoResults = new ArrayList<>();
+        returnedProtoResults.forEach(c ->
+        {
+            PurchaseProto.Purchase purchaseProto = c.value();
+            assertEquals("Customer Null", purchaseProto.getCustomerId());
+            assertEquals(null, purchaseProto.getItem());
+            assertEquals(actualProtoResults.size(), 2);
+        });
+
+        mockConsumer.schedulePollTask(() -> protoConsumerApp.close());
+
+    }
+
+    private <K, V> KeyValue<K, V> toKeyValue(final ProducerRecord<K, V> producerRecord) {
+        return KeyValue.pair(producerRecord.key(), producerRecord.value());
+    }
+
+    private <V> void addTopicPartitionsAssignment(final String topic,
+                                                  final MockConsumer<String, V> mockConsumer) {
+        final TopicPartition topicPartition = new TopicPartition(topic, 0);
+        final Map<TopicPartition, Long> beginningOffsets = new HashMap<>();
+        beginningOffsets.put(topicPartition, 0L);
+        mockConsumer.rebalance(Collections.singletonList(topicPartition));
+        mockConsumer.updateBeginningOffsets(beginningOffsets);
+    }
+
+    private <V> void addConsumerRecords(final MockConsumer<String, V> mockConsumer,
+                                        final List<V> records,
+                                        final Function<V, String> keyFunction,
+                                        final String topic) {
+        AtomicInteger offset = new AtomicInteger(0);
+        records.stream()
+                .map(r -> new ConsumerRecord<>(topic, 0, offset.getAndIncrement(), keyFunction.apply(r), r))
+                .forEach(mockConsumer::addRecord);
+    }
+}
