62eb3ca45d80fe0db70c1636c41cb8b3deff9ff9
Dave Troiano <dtroiano@confluent.io>
2024-02-12 11:05:56 -0500

    producer throughput tutorial

--- optimize-producer-throughput/confluent/code/tutorial-steps/dev/producer-perf-test-default.sh
+++
@@ -1,6 +0,0 @@
-docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:7.3.0 /usr/bin/kafka-producer-perf-test \
-    --topic topic-perf \
-    --num-records 10000 \
-    --record-size 8000 \
-    --throughput -1 \
-    --producer.config /etc/ccloud.properties
