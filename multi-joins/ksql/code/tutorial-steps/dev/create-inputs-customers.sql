692834dd29c3d88be51961405b8f8f32564fefb9
Bill Bejeck <bbejeck@gmail.com>
2024-02-13 17:34:52 -0500

    Converting ksqlDB multi-joins and merging tutorials

--- multi-joins/ksql/code/tutorial-steps/dev/create-inputs-customers.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE customers (customerid STRING PRIMARY KEY, customername STRING) 
-    WITH (KAFKA_TOPIC='customers', 
-          VALUE_FORMAT='json', 
-          PARTITIONS=1);
--- multi-joins/ksql/code/tutorial-steps/dev/create-inputs-orders.sql
+++
@@ -1,4 +0,0 @@
-CREATE STREAM orders (orderid STRING KEY, customerid STRING, itemid STRING, purchasedate STRING) 
-    WITH (KAFKA_TOPIC='orders', 
-          VALUE_FORMAT='json',
-          PARTITIONS=1); 
