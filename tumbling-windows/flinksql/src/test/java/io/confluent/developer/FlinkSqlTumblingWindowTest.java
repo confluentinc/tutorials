c330cceaae2a67a5dfe370a5b902d51873bb2b1d
Bill Bejeck <bbejeck@gmail.com>
2024-02-28 18:43:37 -0500

    Update test cases for FlinkSqlTumblingWindow
    The changes modify test cases for the Flink SQL Tumbling Window by altering the test resources, and updating the Java test code. The modifications are carried towards obtaining and comparing tabular results from the SQL execution, which aims to make test cases more comprehensive and reliable.

--- tumbling-windows/flinksql/src/test/java/io/confluent/developer/FlinkSqlTumblingWindowTest.java
+++ tumbling-windows/flinksql/src/test/java/io/confluent/developer/FlinkSqlTumblingWindowTest.java
@@ -1,12 +1,24 @@
 package io.confluent.developer;
 
 
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.TableResult;
+import org.apache.flink.types.Row;
+import org.apache.flink.types.RowKind;
+import org.apache.flink.util.CloseableIterator;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.apache.flink.table.api.Expressions.$;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FlinkSqlTumblingWindowTest extends AbstractFlinkKafkaTest {
 
@@ -16,14 +28,38 @@ public class FlinkSqlTumblingWindowTest extends AbstractFlinkKafkaTest {
     streamTableEnv.executeSql(getResourceFileContents("create-ratings.sql.template",
         Optional.of(kafkaPort), Optional.of(schemaRegistryPort))).await();
     streamTableEnv.executeSql(getResourceFileContents("populate-ratings.sql")).await();
-
+   
     TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-ratings-by-6hr-window.sql"));
 
-    // Compare actual and expected results. Convert result output to line sets to compare so that order
-    // doesn't matter.
-    String actualTableauResults = tableauResults(tableResult);
-    String expectedTableauResults = getResourceFileContents("expected-ratings-by-6hr-window.txt");
-    assertEquals(stringToLineSet(expectedTableauResults), stringToLineSet(actualTableauResults));
+    List<Row> actualResults = getRowObjectsInList(tableResult);
+
+    List<Row> expectedRowResults = getExpectedFinalUpdateRowObjects();
+    Row actualFirstRow = actualResults.get(0);
+    Row expectedFirstRow = expectedRowResults.get(0);
+    assertEquals(actualFirstRow,expectedFirstRow);
+
+  }
+
+
+  private List<Row> getExpectedFinalUpdateRowObjects() {
+    List<Row> rowList = new ArrayList<>();
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    rowList.add(Row.ofKind(RowKind.INSERT, "Die Hard", 2L, 6.35D, dateTime("2023-07-09 00:00:00",dtf), dateTime("2023-07-09 06:00:00",dtf)));
+    return rowList;
+  }
+
+  private LocalDateTime dateTime(String dateString, DateTimeFormatter formatter){
+    return LocalDateTime.parse(dateString, formatter);
+  }
+
+  private List<Row> getRowObjectsInList(TableResult tableResult) throws Exception {
+    try(CloseableIterator<Row> closeableIterator = tableResult.collect()) {
+      List<Row> rows = new ArrayList<>();
+      while (closeableIterator.hasNext()) {
+        rows.add(closeableIterator.next());
+      }
+      return rows;
+    }
   }
 
 }
