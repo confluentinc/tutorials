8a78b17b0df8433e0e1e3961cb873790f86cd3ae
Bill Bejeck <bbejeck@gmail.com>
2024-04-24 11:58:17 -0400

    feat: Add Top-N Tutorial (#43)
    * Add SQL scripts and unit test for Top-N Flink tutorial
    
    This commit introduces new SQL scripts, a unit test, and related files for a tutorial about how to create a top-N query with Flink SQL.
    
    * Refactor SQL tests to analyze movie views instead of sales
    
    Renamed and updated SQL queries in the test resources to focus on analyzing movie views per genre. These changes allow the computation of a top-N list of movies per genre, based on view counts, instead of the minimum and maximum sales from previous years. The README was also updated to match these changes. Updated dependencies in build.gradle file.
    
    * Update Flink SQL tests and documentation for Top-N queries
    
    The Flink SQL tests and README documentation were updated to reflect a change in the number of top movies we are retrieving based on genre, changing it from top 5 to top 3. Also, the value format for the movie_views table was changed from 'avro-confluent' to 'json'. The structure and the content of the SQL queries were also modified accordingly.
    
    * Apply suggestions from code review
    
    Co-authored-by: Dave Troiano <dtroiano@confluent.io>
    
    * Update wording in README.md
    
    The README document for the top-N FlinkSQL has been updated. The changes improve the explanation about what service or product the user might want to monitor. Now it clearly states that the person might need to monitor some service or product, instead of assuming they have some service or product.
    
    ---------
    
    Co-authored-by: Dave Troiano <dtroiano@confluent.io>

---
+++ top-N/flinksql/src/test/java/io/confluent/developer/FlinkSqlTopNTest.java
@@ -0,0 +1,90 @@
+package io.confluent.developer;
+
+
+import org.apache.flink.table.api.TableResult;
+import org.apache.flink.types.Row;
+import org.apache.flink.types.RowKind;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static io.confluent.developer.TestUtils.rowObjectsFromTableResult;
+import static org.junit.Assert.assertEquals;
+
+public class FlinkSqlTopNTest extends AbstractFlinkKafkaTest {
+
+  @Test
+  public void testTopN() throws Exception {
+    
+    streamTableEnv.getConfig().set("table.exec.source.idle-timeout", "5 ms");
+    streamTableEnv.executeSql(getResourceFileContents("create-movie-views.sql.template",
+        Optional.of(kafkaPort), Optional.of(schemaRegistryPort))).await();
+
+    streamTableEnv.executeSql(getResourceFileContents("populate-movie-views.sql")).await();
+
+    TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-movie-views-for-top-3-dynamic.sql"));
+
+    List<Row> actualResults = rowObjectsFromTableResult(tableResult);
+    List<Row> expectedResults = getExpectedFinalUpdateRowObjects();
+    assertEquals(expectedResults, actualResults);
+  }
+
+  private List<Row> getExpectedFinalUpdateRowObjects() {
+    List<Row> rowList = new ArrayList<>();
+    rowList.add(Row.ofKind(RowKind.INSERT, "The Dark Knight", "Action", 100240L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "The Dark Knight", "Action", 100240L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Avengers: Endgame", "Action", 200010L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "The Dark Knight", "Action", 100240L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Inception", "Sci-Fi", 150000L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Joker", "Drama", 120304L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "The Godfather", "Crime", 300202L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Casablanca", "Romance", 400400L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Joker", "Drama", 120304L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Shawshank Redemption", "Drama", 500056L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Joker", "Drama", 120304L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Joker", "Drama", 120304L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Forrest Gump", "Drama", 350345L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Joker", "Drama", 120304L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Joker", "Drama", 120304L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Fight Club", "Drama", 250250L, 3L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Pulp Fiction", "Crime", 160160L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Pulp Fiction", "Crime", 160160L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Godfather: Part II", "Crime", 170170L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Pulp Fiction", "Crime", 160160L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "The Godfather: Part II", "Crime", 170170L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Departed", "Crime", 180180L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Pulp Fiction", "Crime", 160160L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Godfather: Part II", "Crime", 170170L, 3L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Toy Story 3", "Animation", 190190L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Toy Story 3", "Animation", 190190L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Up", "Animation", 200200L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Toy Story 3", "Animation", 190190L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Up", "Animation", 200200L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Lion King", "Animation", 210210L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Toy Story 3", "Animation", 190190L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Up", "Animation", 200200L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Toy Story 3", "Animation", 190190L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Inception", "Sci-Fi", 150000L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Star Wars: The Force Awakens", "Sci-Fi", 220220L, 1L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Inception", "Sci-Fi", 150000L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Star Wars: The Force Awakens", "Sci-Fi", 220220L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Matrix", "Sci-Fi", 230230L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Inception", "Sci-Fi", 150000L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Star Wars: The Force Awakens", "Sci-Fi", 220220L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Inception", "Sci-Fi", 150000L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "The Matrix", "Sci-Fi", 230230L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Interstellar", "Sci-Fi", 240240L, 1L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Star Wars: The Force Awakens", "Sci-Fi", 220220L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "The Matrix", "Sci-Fi", 230230L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Inception", "Sci-Fi", 150000L, 3L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Star Wars: The Force Awakens", "Sci-Fi", 220220L, 3L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Titanic", "Romance", 250250L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_BEFORE, "Titanic", "Romance", 250250L, 2L));
+    rowList.add(Row.ofKind(RowKind.UPDATE_AFTER, "Pride and Prejudice", "Romance", 260260L, 2L));
+    rowList.add(Row.ofKind(RowKind.INSERT, "Titanic", "Romance", 250250L, 3L));
+    return rowList;
+  }
+
+}
