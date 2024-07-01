553b49bdb62fc4a871f60bbf013fcbc1a5ecb845
Bill <bill@confluent.io>
2024-07-01 15:14:30 -0400

    Add KStreams application to join movie and rating streams
    The commit introduces a Kafka Streams (KStreams) application named `JoinStreamToStream` which joins two streams of data; movies and ratings. This application uses `KStream.join` operation to combine each rating with the corresponding movie. This commit also includes the testing of this joining operation. Movie and rating data models are also added along with a `MovieRatingJoiner` to specify how to construct the joined value of both streams.

---
+++ joining-stream-stream/kstreams/src/main/java/io/confluent/developer/JoinStreamToStream.java
@@ -0,0 +1,75 @@
+package io.confluent.developer;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class JoinStreamToStream {
+     private static final Logger LOG = LoggerFactory.getLogger(JoinStreamToStream.class);
+
+     public static final String MOVIE_INPUT_TOPIC = "movie-input";
+     public static final String RATING_INPUT_TOPIC = "ratings-input";
+     public static final String RATED_MOVIES_OUTPUT = "rated-movies-output";
+
+
+    public Topology buildTopology(Properties allProps) {
+        
+        final StreamsBuilder builder = new StreamsBuilder();
+        final MovieRatingJoiner joiner = new MovieRatingJoiner();
+        final Serde<Movie> movieSerde = new StreamsSerde<>(Movie.class);
+        final Serde<Rating> ratingSerde = new StreamsSerde<>(Rating.class);
+        final Serde<RatedMovie> ratedMovieSerde = new StreamsSerde<>(RatedMovie.class);
+        final JoinWindows joinWindows = JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(10));
+
+        KStream<Long, Movie> movieStream = builder.stream(MOVIE_INPUT_TOPIC,
+                        Consumed.with(Serdes.Long(), movieSerde))
+                .peek((key, value) -> LOG.info("Incoming movies key[{}] value[{}]", key, value));
+
+        KStream<Long, Rating> ratings = builder.stream(RATING_INPUT_TOPIC,
+                        Consumed.with(Serdes.Long(), ratingSerde))
+                .peek((key, value) -> LOG.info("Incoming ratings key[{}] value[{}]", key, value))
+                .map((key, rating) -> new KeyValue<>(rating.id(), rating));
+
+        ratings.join(movieStream, joiner, joinWindows, StreamJoined.with(Serdes.Long(),ratingSerde, movieSerde))
+                .peek((key, value) -> LOG.info("Joined results key[{}] value[{}]", key, value))
+                .to(RATED_MOVIES_OUTPUT,
+                        Produced.with(Serdes.Long(), ratedMovieSerde));
+
+        return builder.build(allProps);
+    }
+
+
+    public static void main(String[] args) {
+        Properties properties;
+        if (args.length > 0) {
+            properties = Utils.loadProperties(args[0]);
+        } else {
+            properties = Utils.loadProperties();
+        }
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "join-stream-to-stream");
+        JoinStreamToStream joinStreamToStream = new JoinStreamToStream();
+
+        Topology topology = joinStreamToStream.buildTopology(properties);
+
+        try (KafkaStreams kafkaStreams = new KafkaStreams(topology, properties)) {
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                kafkaStreams.close(Duration.ofSeconds(5));
+                countDownLatch.countDown();
+            }));
+            // For local running only don't do this in production as it wipes out all local state
+            kafkaStreams.cleanUp();
+            kafkaStreams.start();
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
