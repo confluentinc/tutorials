34b9039074ac0d8f37aaa98d6d03fff7f0530def
Dave Troiano <dtroiano@confluent.io>
2024-12-03 14:06:21 -0500

    feat: Flink SQL array expansion tutorial

---
+++ array-expansion/flinksql/src/test/resources/populate-traveler-locations.sql
@@ -0,0 +1,4 @@
+INSERT INTO traveler_locations VALUES
+    (0, 'Jane Smith', ARRAY['Rome', 'Paris']),
+    (1, 'Xander Jackson', ARRAY['Berlin', 'Paris']),
+    (2, 'Sally Stewart', ARRAY['Lisbon']);
