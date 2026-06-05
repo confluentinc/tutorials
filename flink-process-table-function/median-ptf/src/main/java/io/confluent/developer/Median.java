70f62702f3de114de75338876866016bf142d949
Dave Troiano <dtroiano@confluent.io>
2026-06-05 14:56:15 -0400

    chore: Flink PTF tutorial improvements
    * add instruction to clean up CC artifact key
    * modify PTF to leverage type inference more
    * explain annotations and type inference
    * explain when artifact upload is required

--- flink-process-table-function/median-ptf/src/main/java/io/confluent/developer/Median.java
+++ flink-process-table-function/median-ptf/src/main/java/io/confluent/developer/Median.java
@@ -2,7 +2,6 @@ package io.confluent.developer;
 
 import com.google.common.math.Quantiles;
 import org.apache.flink.table.annotation.ArgumentHint;
-import org.apache.flink.table.annotation.DataTypeHint;
 import org.apache.flink.table.annotation.StateHint;
 import org.apache.flink.table.functions.ProcessTableFunction;
 import org.apache.flink.types.Row;
@@ -12,24 +11,36 @@ import java.util.List;
 
 import static org.apache.flink.table.annotation.ArgumentTrait.SET_SEMANTIC_TABLE;
 
-@DataTypeHint("ROW<temperature DOUBLE, median DOUBLE>")
-public class Median extends ProcessTableFunction<Row> {
+public class Median extends ProcessTableFunction<Median.MedianResult> {
     public static class TempsState {
         public List<Double> temps = new ArrayList<>();
     }
 
+    // Median calculation POJO
+    public static class MedianResult {
+        public double temperature;
+        public double median;
+
+        public static MedianResult of(double temperature, double median) {
+            MedianResult result = new MedianResult();
+            result.temperature = temperature;
+            result.median = median;
+            return result;
+        }
+    }
+
     public void eval(
             @StateHint TempsState trailingTemps,
-            @ArgumentHint(SET_SEMANTIC_TABLE) Row row,
-            @DataTypeHint("INT") Integer numTrailing
+            @ArgumentHint(name = "input", value = SET_SEMANTIC_TABLE) Row input,
+            @ArgumentHint(name = "numTrailing") int numTrailing
     ) {
-        Double temperature = row.getFieldAs("temperature");
+        Double temperature = input.getFieldAs("temperature");
 
         trailingTemps.temps.add(temperature);
         while (trailingTemps.temps.size() > numTrailing) {
             trailingTemps.temps.remove(0);
         }
 
-        collect(Row.of(temperature, Quantiles.median().compute(trailingTemps.temps)));
+        collect(MedianResult.of(temperature, Quantiles.median().compute(trailingTemps.temps)));
     }
 }
